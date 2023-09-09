8509412c6727efb931f67196b5db8d984787feef
pasandevin <jayawardene.pasandevin@gmail.com>
2023-09-09 21:13:28 +0530

    Added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { JourneysModule } from './journeys/journeys.module';
 
 @Module({
-  imports: [],
+  imports: [MongooseModule.forRoot('MONOGODB_URL'), JourneysModule],
   controllers: [AppController],
   providers: [AppService],
 })
