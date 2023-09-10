9685910148dfe79bb388a934620ec354ae2fbe07
pasandevin <jayawardene.pasandevin@gmail.com>
2023-09-11 00:19:14 +0530

    Removed validation on journey description

--- src/journeys/dto/journeyCreate.dto.ts
+++ src/journeys/dto/journeyCreate.dto.ts
@@ -3,7 +3,6 @@ import { IsNotEmpty, IsString } from 'class-validator';
 export class JourneyCreateDto {
   @IsNotEmpty()
   title: string;
-  @IsString()
   description: string;
   @IsNotEmpty()
   userName: string;
