a4f7ccb6022f641b0127c9b38c560bf652c57872
Krishan Shamod <krishanshamod@gmail.com>
2023-09-12 22:27:22 +0530

    Added JWT Authentication

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
 import { AwsCognitoService } from './aws-cognito.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  providers: [AwsCognitoService],
+  imports: [PassportModule.register({ defaultStrategy: 'jwt' })],
+  providers: [AwsCognitoService, JwtStrategy],
   controllers: [AuthController],
 })
 export class AuthModule {}
