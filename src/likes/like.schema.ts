a56288937d0efbad8061c824e694bcf47cfe393e
pasandevin <jayawardene.pasandevin@gmail.com>
2023-09-19 19:39:40 +0530

    Created likes module

---
+++ src/likes/like.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { SMPost } from 'src/posts/smPost.schema';
+import { User } from 'src/users/user.schema';
+
+export type LikeDocument = Like & Document;
+
+@Schema()
+export class Like {
+  @Prop({ default: Date.now })
+  createdAt: Date;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User' })
+  user: User;
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'SmPost' })
+  smPost: SMPost;
+}
+
+export const LikeSchema = SchemaFactory.createForClass(Like);
---
+++ src/likes/likeSmPost.dto.ts
@@ -0,0 +1,8 @@
+import { IsEmail, IsNotEmpty } from "class-validator";
+
+export class LikeSmPostDto {
+  @IsNotEmpty()
+  smPostId: string;
+  @IsEmail()
+  userEmail: string;
+}
