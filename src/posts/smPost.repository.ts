bc2432d7aa069a1862b82380bc0b4609e6394312
pasandevin <jayawardene.pasandevin@gmail.com>
2023-09-22 15:00:07 +0530

    Moved the relavant logics to post service

--- src/posts/smPost.repository.ts
+++ src/posts/smPost.repository.ts
@@ -1,18 +1,11 @@
-import {
-  Injectable,
-  NotAcceptableException,
-  NotFoundException,
-} from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { SMPost } from './smPost.schema';
 import mongoose, { Model } from 'mongoose';
 import { SMPostCreateDto } from './dto/smPostCreate.dto';
 import { SMPostUpdateDto } from './dto/smPostUpdate.dto';
-import { LikeSmPostDto } from 'src/likes/likeSmPost.dto';
 import { Like } from 'src/likes/like.schema';
 import { User } from 'src/users/user.schema';
-import { LikesService } from 'src/likes/likes.service';
-import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class SMPostRepository {
@@ -20,8 +13,6 @@ export class SMPostRepository {
     @InjectModel(SMPost.name) private smPostModel: Model<SMPost>,
     @InjectModel(Like.name) private likeSmPostModel: Model<Like>,
     @InjectModel(User.name) private userModel: Model<User>,
-    private likeService: LikesService,
-    private userService: UsersService,
   ) {}
 
   async createSMPost(smPostCreateDto: SMPostCreateDto): Promise<SMPost> {
@@ -58,63 +49,10 @@ export class SMPostRepository {
     );
   }
 
-  async likeAPost(likeSmPostDto: LikeSmPostDto): Promise<SMPost> {
-    const post = await this.findById(likeSmPostDto.smPostId);
-    if (!post) {
-      throw new NotFoundException('Post not found');
-    }
-
-    //find if user already exists
-    // let user: User = await this.userModel.findOne({
-    //   userEmail: likeSmPostDto.userEmail,
-    // });
-
-    let user: User = await this.userService.findAUser(likeSmPostDto.userEmail);
-
-    //if user doesn't exist, create a new user
-    // if (!user) {
-    //   user = await new this.userModel({
-    //     userEmail: likeSmPostDto.userEmail,
-    //   }).save();
-    // }
-
-    if (!user) {
-      user = await this.userService.createUser(likeSmPostDto.userEmail);
-    }
-
-    // Check if the user has already liked the post
-    // const existingLike = await this.likeSmPostModel.findOne({
-    //   user: user._id,
-    //   smPost: likeSmPostDto.smPostId,
-    // });
-    // if (existingLike) {
-    //   throw new NotAcceptableException('User has already liked this post');
-    // }
-
-    const existingLike = await this.likeService.findALike(
-      user._id,
-      likeSmPostDto.smPostId,
-    );
-    if (existingLike) {
-      throw new NotAcceptableException('User has already liked this post');
-    }
-
-    //Create a new like
-    // const newLike = await new this.likeSmPostModel({
-    //   user: user._id,
-    //   smPost: likeSmPostDto.smPostId,
-    // }).save();
-
-    const newLike = await this.likeService.createLike(
-      user._id,
-      likeSmPostDto.smPostId,
-    );
-
-    // Add the like to the post's likes array
-    // return await this.smPostModel.findByIdAndUpdate(
-    //   { _id: likeSmPostDto.smPostId },
-    //   { $push: { likes: newLike._id } },
-    // );
-    return await this.addALikeToAPost(likeSmPostDto.smPostId, newLike._id);
+  async likeAPost(
+    smPostId: string,
+    newLikeId: mongoose.Types.ObjectId,
+  ): Promise<SMPost> {
+    return await this.addALikeToAPost(smPostId, newLikeId);
   }
 }
--- src/posts/smPosts.service.ts
+++ src/posts/smPosts.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {
+  Injectable,
+  NotAcceptableException,
+  NotFoundException,
+} from '@nestjs/common';
 import { SMPostCreateDto } from './dto/smPostCreate.dto';
 import { SMPost } from './smPost.schema';
 import { SMPostRepository } from './smPost.repository';
 import { SMPostUpdateDto } from './dto/smPostUpdate.dto';
 import { LikeSmPostDto } from 'src/likes/likeSmPost.dto';
+import { LikesService } from 'src/likes/likes.service';
+import { UsersService } from 'src/users/users.service';
+import { User } from 'src/users/user.schema';
 
 @Injectable()
 export class SMPostsService {
-  constructor(private smPostRepository: SMPostRepository) {}
+  constructor(
+    private smPostRepository: SMPostRepository,
+    private likeService: LikesService,
+    private userService: UsersService,
+  ) {}
 
   async createSMPost(smPostCreateDto: SMPostCreateDto): Promise<SMPost> {
     return await this.smPostRepository.createSMPost(smPostCreateDto);
@@ -23,6 +34,33 @@ export class SMPostsService {
   }
 
   async likeAPost(likeSmPostDto: LikeSmPostDto): Promise<SMPost> {
-    return await this.smPostRepository.likeAPost(likeSmPostDto);
+    const post = await this.smPostRepository.findById(likeSmPostDto.smPostId);
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+
+    let user: User = await this.userService.findAUser(likeSmPostDto.userEmail);
+
+    if (!user) {
+      user = await this.userService.createUser(likeSmPostDto.userEmail);
+    }
+
+    const existingLike = await this.likeService.findALike(
+      user._id,
+      likeSmPostDto.smPostId,
+    );
+    if (existingLike) {
+      throw new NotAcceptableException('User has already liked this post');
+    }
+
+    const newLike = await this.likeService.createLike(
+      user._id,
+      likeSmPostDto.smPostId,
+    );
+
+    return await this.smPostRepository.likeAPost(
+      likeSmPostDto.smPostId,
+      newLike._id,
+    );
   }
 }
